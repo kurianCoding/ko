6a613e21e51b4010cb8c72498fb46a9271e1ead9
kurian <kurianck.mail@gmail.com>
2018-09-20 09:04:46 +0530

    making command line application
        making the skeleton for cmd,
    
        TODO: find the package
        TODO: find the list of functions

--- cmd.go
+++ cmd.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"strings"
+	//"reflect"
 )
 
 /*
@@ -20,37 +22,48 @@ func GetFunctionString(functions []string) string {
 	}
 	return functionString
 }
+
+//TODO: this function returns the header that is to be written
+// to the ko test
 func ImportString() string {
-	var ImportString string
-	ImportString = fmt.Sprintf("%s\n", reflect.TypeOf(Empty{}).PkgPath())
-	return `import "testing"` + ImportString
+	return `import "testing"`
 }
 
 //TODO: this function returns a list of all the functions that are called
 //while testing in the current directory
-func ListFunctions() []strings {
+func ListFunctions() []string {
+
+	files := []string{"Test", "Test1", "Test2"}
+	return files
 
 }
 
 /*
 this function reads the file .testignore and reads
 the function which are to be ignored*/
-func ExcludeList() map[string]bool {
+func ExcludeList() (map[string]bool, error) {
 	//read contents from file
-	f := os.Open(".testignore", os.READ)
+	f, err := os.Open(".testignore")
+	if err != nil {
+		return nil, err
+	}
 	fileContent, err := ioutil.ReadAll(f)
-	functionString = string(fileContent)
+	if err != nil {
+		return nil, err
+	}
+	functionString := string(fileContent)
 	exfunctions := strings.Split(functionString, "\n")
-	exmap = make(map[string]bool)
-	for key, val := range exfunctions {
+	var exmap = make(map[string]bool)
+	for _, val := range exfunctions {
 		exmap[val] = true
 	}
-	return exmap
+	return exmap, nil
 }
 
+/* write content to temporary go file for ko testing*/
 func WriteContent(cont []byte) error {
 	// open file
-	f, err := os.Open("ko_test.go", os.Create || os.RDWR || os.Write, 655)
+	f, err := os.OpenFile("ko_test.go", os.O_APPEND|os.O_RDWR|os.O_CREATE, 655)
 	if err != nil {
 		return err
 	}
@@ -65,28 +78,39 @@ func WriteContent(cont []byte) error {
 	return nil
 }
 
+/*invoking go test*/
 func CmdExecTest() error {
 	cmd := exec.Command("go", "test", "ko_test.go")
 	err := cmd.Run()
 	if err != nil {
 		return err
 	}
+	return nil
 }
 
+/*remove file*/
+
 func RemoveFile() error {
-	// remove file
+	_, err := os.Stat("ko_test.go")
+	if os.IsNotExist(err) {
+		return nil
+	}
 	cmd := exec.Command("rm", "ko_test.go")
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		return err
 	}
+	return nil
 }
 
 func main() {
 	functions := ListFunctions()
-	exfunctions := ExcludedList()
-	preparefunctions = make([]string)
-	for key, val := range functions {
+	exfunctions, err := ExcludeList()
+	if err != nil {
+		panic(err)
+	}
+	var preparefunctions = make([]string, 1)
+	for _, val := range functions {
 		if !exfunctions[val] {
 			preparefunctions = append(preparefunctions, val)
 		}
@@ -97,7 +121,7 @@ func main() {
 		panic(err)
 	}
 
-	defer RemoveFile() // remove the file after testing
+	defer RemoveFile() // remove the file after checking if it exists
 	err = CmdExecTest()
 
 	if err != nil {
